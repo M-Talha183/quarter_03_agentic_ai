d62f2e68de190ca58720f0e6abfb7f7cdcdc565f
Muhammad361738 <ullahehsan016@gmail.com>
2025-06-11 04:29:54 +0500

    Add new agent with open routter

---
+++ agent_with_open_router/main.py
@@ -0,0 +1,40 @@
+from dotenv import load_dotenv
+import os 
+from agents import Agent , AsyncOpenAI, OpenAIChatCompletionsModel,RunConfig, Runner
+
+load_dotenv()
+open_router_key = os.getenv("OPENROUTER_API_KEY")
+# Check if the API key is present; if not, raise an error
+if not open_router_key:
+    raise ValueError("OPENROUTER_API_KEY is not set. Please ensure it is defined in your .env file.")
+
+#Reference: https://ai.google.dev/gemini-api/docs/openai
+external_client = AsyncOpenAI(
+    api_key= open_router_key,
+    base_url="https://openrouter.ai/api/v1",
+)
+
+model = OpenAIChatCompletionsModel(
+    model="deepseek/deepseek-r1-0528:free",
+    openai_client=external_client
+)
+
+config = RunConfig(
+    model=model,
+    model_provider=external_client,
+    tracing_disabled=True
+)
+     
+agent = Agent(
+    name = "Teacher",
+    instructions = "You are a Python Teacher."
+)
+
+prompt = input("Hello, How can i assist you?\n")
+
+responce = Runner.run_sync(
+    agent,
+    input = prompt,
+    run_config = config
+    )
+print(responce) 
