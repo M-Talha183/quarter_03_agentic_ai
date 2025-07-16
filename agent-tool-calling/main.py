e6a410d1e17d3d95fef09b6567d02ed882f9cda1
Muhammad361738 <ullahehsan016@gmail.com>
2025-07-17 03:03:58 +0500

    update main.py file create basic code for tool calling agent

--- agent-tool-calling/main.py
+++ agent-tool-calling/main.py
@@ -1,6 +1,26 @@
-def main():
-    print("Hello from agent-tool-calling!")
+import os 
+from dotenv import load_dotenv
+from openai import AsyncOpenAI
+from agents import Agent , RunConfig , Runner , OpenAIChatCompletionsModel ,set_tracing_disabled
 
+load_dotenv()
+set_tracing_disabled(True)
 
-if __name__ == "__main__":
-    main()
+gemini_api_key = os.getenv("GEMINI_API_KET")
+
+if not gemini_api_key :
+    raise ValueError("Gimini api key is not sset ! Please set first it ")
+
+provider = AsyncOpenAI(
+    api_key= gemini_api_key,
+    base_url= "https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+model = OpenAIChatCompletionsModel(
+    model= "gemini-2.0-flash",
+    openai_client=provider
+)
+config =  RunConfig(
+    model=model,
+    model_provider=provider # type:ignore
+)
