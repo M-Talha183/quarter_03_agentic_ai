1f93e54a31997880cc39af1c27e4cf25310fa691
Muhammad361738 <ullahehsan016@gmail.com>
2025-07-25 22:31:17 +0500

    Add new project

---
+++ agent-as-tool/connection.py
@@ -0,0 +1,26 @@
+import os 
+from dotenv import load_dotenv
+from openai import AsyncOpenAI
+from agents import  RunConfig  , OpenAIChatCompletionsModel ,set_tracing_disabled
+
+load_dotenv()
+set_tracing_disabled(True)
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+if not gemini_api_key :
+    raise ValueError("Gimini api key is not sset ! Please set first it ")
+
+provider = AsyncOpenAI(
+    api_key= gemini_api_key,
+    base_url= "https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+model = OpenAIChatCompletionsModel(
+    model= "gemini-2.0-flash",
+    openai_client=provider
+)
+config =  RunConfig(
+    model=model,
+    model_provider=provider # type:ignore
+)
