8361e1475a681ce89052a44777172b5f3007b2fe
Muhammad361738 <ullahehsan016@gmail.com>
2025-08-15 09:30:21 +0500

    update guadrial with tracing.py

---
+++ guardrails/tracing.py
@@ -0,0 +1,36 @@
+import os
+from dotenv import load_dotenv
+from openai import AsyncOpenAI
+from agents import Agent , OpenAIChatCompletionsModel , Runner , set_tracing_export_api_key
+
+load_dotenv()
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+open_ai_api_key = os.getenv("OPEN_AI_API_KEY")
+
+set_tracing_export_api_key(open_ai_api_key)
+
+if not gemini_api_key:
+    raise ValueError("Gemini api key can work please check it ")
+
+provider = AsyncOpenAI(
+    api_key= gemini_api_key,
+    base_url= "https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+model = OpenAIChatCompletionsModel(
+    model= "gemini-2.0-flash",
+    openai_client=provider
+)
+
+agent = Agent (
+    name = "Help ful agent",
+    instructions="You are a helpful assistant.",
+    model=model
+)
+
+result = Runner.run_sync(
+    starting_agent=agent,
+    input= "What is the capital of France?"
+)
+print(result.final_output)
